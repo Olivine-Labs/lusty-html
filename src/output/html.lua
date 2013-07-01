6b87ffb2d88804227cee83c41e2d3d25937a7ace
Drew Ditthardt <dditthardt@olivinelabs.com>
2013-07-01 16:45:50 -0700

    Made html output more consistent and less error prone.

--- src/output/html.lua
+++ src/output/html.lua
@@ -2,14 +2,7 @@ return {
   handler = function(context)
     context.response.headers["content-type"] = "text/html"
 
-    local output = context.output
-    local meta = getmetatable(output)
-
-    if meta and type(meta.__toView) == "function" then
-      output = meta.__toView(output, context)
-    end
-
-    context.response.send(output)
+    context.response.send(context.output)
   end,
 
   options = {
@@ -21,8 +14,9 @@ return {
       local accept = context.request.headers.accept or "text/html"
       local content = context.request.headers["content-type"]
 
-      return (accept and (accept:find("text/html") or accept:find("*/*"))) or
-             (content and content:find("text/html"))
+      return ((accept and (accept:find("text/html") or accept:find("*/*"))) or
+             (content and content:find("text/html"))) and
+             type(context.output) == "string"
     end
   }
 }
